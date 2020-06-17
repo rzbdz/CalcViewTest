fce880be8931884bb07a2957f92abe7d6353943e
RzBu11d023R <18226542+PanJunzhong@users.noreply.github.com>
2020-06-17 21:46:24 +0800

    添加右键菜单和全选功能&修复Bugs

--- src/Header.java
+++ src/Header.java
@@ -3,35 +3,97 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.event.MenuKeyEvent;
+import javax.swing.event.MenuKeyListener;
 import javax.swing.text.Document;
 import java.awt.*;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.TextEvent;
-import java.awt.event.TextListener;
+import java.awt.event.*;
 
 class Header extends JPanel {
     JTextField resultTextField;
     JTextField processTextField;
+    JPopupMenu rightResultPopupMenu;
+    JMenuItem jmitCopy;
+    JMenuItem jmitPaste;
+    JMenuItem jmitSeleteAll;
 
     Header() {
+        //显示算式的文本框
+        processTextField = new JTextField();
+        processTextField.setHorizontalAlignment(JTextField.RIGHT);
+        processTextField.setEditable(false);
+        processTextField.setFocusable(true);
+        processTextField.setFont(new BasicFont(Font.PLAIN, 12));
+        processTextField.setForeground(Color.gray);
+        processTextField.setText("8+5");
+        processTextField.setBorder(new EmptyBorder(15, 5, 0, 5));
+        //显示当前按下数字和计算结果的文本框
         resultTextField = new JTextField();
         resultTextField.setHorizontalAlignment(JTextField.RIGHT);
         resultTextField.setEditable(false);
         resultTextField.setFocusable(true);
         resultTextField.setFont(new BasicFont(Font.BOLD, 36));
         resultTextField.setBorder(new EmptyBorder(0, 5, 0, 5));
+        //定义这个文本框的右键弹出菜单
+        rightResultPopupMenu = new JPopupMenu();
+        jmitCopy = new JMenuItem("复制");
+        jmitPaste = new JMenuItem("粘贴");
+        jmitSeleteAll = new JMenuItem("全选");
+        jmitSeleteAll.addActionListener(new PopupMenuListener());
+        rightResultPopupMenu.add(jmitCopy);
+        rightResultPopupMenu.add(jmitPaste);
+        rightResultPopupMenu.add(jmitSeleteAll);
+        //构造鼠标右键监听器
+        resultTextField.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                //Button3就是鼠标右键
+                if (e.getButton() == MouseEvent.BUTTON3) {
+                    rightResultPopupMenu.show(Header.this, e.getX(), e.getY());
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
         //构造一个文本监听器,当文本长度长于窗口宽度时调整字体大小
         resultTextField.getDocument().addDocumentListener(new DocumentListener() {
             private void filter(DocumentEvent e) {
                 System.out.println(e.getDocument().getLength());
                 ChangeFont cf;
-                if (e.getDocument().getLength() >= 17) {
-                    System.out.println("locked header");
-                    cf = new ChangeFont(new BasicFont(Font.BOLD, 24));
+                if (e.getDocument().getLength() >= 20) {
+                    //某个长度的时候字号设置为超小
+                    cf = new ChangeFont(new BasicFont(Font.BOLD, 20));
+                } else if (e.getDocument().getLength() >= 16) {
+                    //某个长度的时候字号设置为最小
+                    cf = new ChangeFont(new BasicFont(Font.BOLD, 26));
+                } else if (e.getDocument().getLength() >= 14) {
+                    //某个长度的时候字号设置为中等
+                    cf = new ChangeFont(new BasicFont(Font.BOLD, 30));
                 } else {
-                    System.out.println("locked header");
-                    cf= new ChangeFont(new BasicFont(Font.BOLD, 36));
+                    //某个长度的时候字号设置为最大
+                    cf = new ChangeFont(new BasicFont(Font.BOLD, 36));
                 }
+                //稍后执行changeFont操作
+                //这里这样调用是因为使用JTextField进入了Document部分,
+                //JTextField被加锁
                 EventQueue.invokeLater(cf);
             }
 
@@ -52,34 +114,30 @@ class Header extends JPanel {
 
         });
 
-        processTextField = new JTextField();
-        processTextField.setHorizontalAlignment(JTextField.RIGHT);
-        processTextField.setEditable(false);
-        processTextField.setFocusable(true);
-        processTextField.setFont(new BasicFont(Font.PLAIN, 12));
-        processTextField.setForeground(Color.darkGray);
-        processTextField.setText("8+5");
-        processTextField.setBorder(new EmptyBorder(15, 5, 0, 5));
-
+        //排版两个文本框的GridBagLayout
         GridBagLayout textFieldGridBagLayout = new GridBagLayout();
         setLayout(textFieldGridBagLayout);
         GridBagConstraints c = new GridBagConstraints();
+        //垂直占比10分之一,水平扩展100%
         c.fill = GridBagConstraints.BOTH;
         c.weightx = 1;
         c.weighty = 0.1;
         c.gridx = 0;
         c.gridy = 0;
         textFieldGridBagLayout.setConstraints(processTextField, c);
+        //垂直占比10分之9,水平扩展100%
         c.weightx = 1;
         c.weighty = 0.9;
         c.gridx = 0;
         c.gridy = 1;
         textFieldGridBagLayout.setConstraints(resultTextField, c);
+        //添加两个文本框
         add(processTextField);
         add(resultTextField);
     }
 
-    //为上面的文本监听器服务
+    //为上面的文本监听器服务,稍后执行的Runable接口实现
+    //用来修改resultTextField的文字大小
     private class ChangeFont implements Runnable {
         Font ft;
 
@@ -90,6 +148,18 @@ class Header extends JPanel {
         @Override
         public void run() {
             Header.this.resultTextField.setFont(ft);
+        }
+    }
+
+    private class PopupMenuListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == jmitSeleteAll){
+                System.out.println("press select all");
+                Header.this.resultTextField.requestFocus();
+                Header.this.resultTextField.selectAll();
+            }
         }
     }
 }
