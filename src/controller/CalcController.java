a4d32f912129823926a4cff641ced2f1cf624ec5
RzBu11d023R <18226542+PanJunzhong@users.noreply.github.com>
2020-06-27 23:54:29 +0800

    some dev ver structure

--- src/controller/CalcController.java
+++ src/controller/CalcController.java
@@ -2,12 +2,46 @@ package controller;
 
 import model.Calculation;
 
+import java.math.BigDecimal;
+
 public class CalcController {
     Calculation model = new Calculation();
-    public static enum BINARY_SYMBOl {TIMES,PLUS,DIVIDES,MINUS,EQUALS,MOD};
-    private  BINARY_SYMBOl binary_symbol;
-    public static enum UNARY_SYMBOl {ONE_DIVIDES_X,X_SQUARE,SQRT};
+    static CalcController controller=new CalcController();
+    public static CalcController getInstance(){
+        return controller;
+    }
+    private CalcController(){
+
+    }
+    String expression="900+900";
+    char operator='=';
+    BigDecimal result=new BigDecimal("1800");
+
+    public char getOperator() {
+        return operator;
+    }
+
+    public String getExpression() {
+        return expression;
+    }
+
+    public BigDecimal getResult() {
+        return result;
+    }
+
+
+    public static enum BINARY_SYMBOl {TIMES, PLUS, DIVIDES, MINUS, EQUALS, MOD}
+
+    ;
+    private BINARY_SYMBOl binary_symbol;
+
+    public static enum UNARY_SYMBOl {ONE_DIVIDES_X, X_SQUARE, SQRT}
+
+    ;
     private UNARY_SYMBOl unary_symbol;
-    public static enum CURRENT_STATE{};
+
+    public static enum CURRENT_STATE {}
+
+    ;
     private CURRENT_STATE current_state;
 }
